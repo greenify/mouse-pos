e86acee17d4f54a5320a42389ca3eccf506eb8a0
Sebastian Wilzbach <seb@wilzba.ch>
2015-02-21 03:19:35 +0100

    fix bug in the mouse wheel function

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ module.exports = Mouse = {
     var mouseX, mouseY, rect, target;
     mouseX = e.offsetX;
     mouseY = e.offsetY;
-    if (mouseX == null) {
+    if (mouseX == undefined) {
       rect = target.getBoundingClientRect();
       target = e.target || e.srcElement;
-      if (mouseX == null) {
+      if (mouseX == undefined) {
         mouseX = e.clientX - rect.left;
         mouseY = e.clientY - rect.top;
       }
-      if (mouseX == null) {
+      if (mouseX == undefined) {
         mouseX = e.pageX - target.offsetLeft;
         mouseY = e.pageY - target.offsetTop;
       }
-      if (mouseX == null) {
+      if (mouseX == undefined) {
         console.log(e, "no mouse event defined. your browser sucks");
         return;
       }
@@ -27,26 +27,35 @@ module.exports = Mouse = {
     var mouseX, mouseY;
     mouseX = e.pageX;
     mouseY = e.pageY;
-    if (mouseX == null) {
+    if (mouseX == undefined) {
       mouseX = e.layerX;
       mouseY = e.layerY;
     }
-    if (mouseX == null) {
+    if (mouseX == undefined) {
       mouseX = e.clientX;
       mouseY = e.clientY;
     }
-    if (mouseX == null) {
+    if (mouseX == undefined) {
       mouseX = e.x;
       mouseY = e.y;
     }
     return [mouseX, mouseY];
   },
   wheelDelta: function(e) {
-    var delta, dir;
+    var delta;
     delta = [e.deltaX, e.deltaY];
-    if (delta[0] == null) {
-      dir = Math.floor(e.detail / 3);
-      delta = [0, e.mozMovementX * dir];
+    if (delta[0] == undefined) {
+      // in case there is a more detailed scroll sensor - use it
+      if (e.mozMovementX) {
+        delta = [0, e.mozMovementX];
+      }
+    }
+    // safety first
+    if (isNaN(delta[0])) {
+      delta[0] = 0;
+    }
+    if (isNaN(delta[1])) {
+      delta[1] = 0;
     }
     return delta;
   }
